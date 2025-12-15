889e34478c9d1d84848bd634f495b6285b00ad4e
HossainAraf <arafat.kd99@gmail.com>
2025-12-15 12:35:39 +0600

    feat(contact_submission): Add enum for status

--- app/models/contact_submission.rb
+++ app/models/contact_submission.rb
@@ -2,4 +2,11 @@ class ContactSubmission < ApplicationRecord
     // Validations
     validates :name, :email, :message,  presence: true
     validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+
+    enum status: {
+        new: 'new',
+        read: 'read',
+        replied: 'replied',
+        archived: 'archived'
+    }, default: 'new'
 end
